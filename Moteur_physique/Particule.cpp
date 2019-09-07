c49bf29e0bcb28fd79c423a54cd1f4a35bbce5f5
Martin <martin.chapuis@live.fr>
2019-09-07 18:44:24 -0400

    Correction de la couleur des particules.

--- Moteur_physique/Particule.cpp
+++ Moteur_physique/Particule.cpp
@@ -1,6 +1,6 @@
 #include "Particule.h"
 
-Particule::Particule(float g, Vecteur3D* pos, Vecteur3D vit, float m, float d, float r, float v, float b) : g_(0, 0, -g), pos_(pos), vit_(vit), massInv_(1/m), d_(d), r_(d), v_(d), b_(d), m_(m)
+Particule::Particule(float g, Vecteur3D* pos, Vecteur3D vit, float m, float d, float r, float v, float b) : g_(0, 0, -g), pos_(pos), vit_(vit), massInv_(1/m), d_(d), r_(r), v_(v), b_(b), m_(m)
 {
 }
 
