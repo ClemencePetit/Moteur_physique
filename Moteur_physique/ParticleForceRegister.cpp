bd37efff6fafa94b3d8877a2afbc01c12cdf3cf2
Martin <martin.chapuis@live.fr>
2019-09-18 15:03:06 -0400

    Ajout du registre

---
+++ Moteur_physique/ParticleForceRegister.cpp
@@ -0,0 +1,31 @@
+#include "ParticleForceRegister.h"
+
+ParticleForceRegister::ParticleForceRegister()
+{
+}
+
+
+ParticleForceRegister::~ParticleForceRegister()
+{
+}
+
+void ParticleForceRegister::add(Particle* p, ParticleForceGenerator* fg) {
+	ForceRecord fr;
+	fr.fg = fg;
+	fr.p = p;
+	register_.push_back(fr);
+}
+
+void ParticleForceRegister::remove() {
+	register_.pop_back();
+}
+
+void ParticleForceRegister::clear() {
+	register_.clear();
+}
+
+void ParticleForceRegister::updateForces() {
+	for (auto it = register_.begin(); it != register_.end(); it++)
+	{
+	}
+}
---
+++ Moteur_physique/ParticleForceRegister.h
@@ -0,0 +1,29 @@
+#ifndef PARTICLEFORCEREGISTER_H
+#define PARTICLEFORCEREGISTER_H
+
+#include <vector>
+#include "Particle.h"
+#include "ParticleForceGenerator.h"
+
+struct ForceRecord {
+	Particle* p;
+	ParticleForceGenerator* fg;
+};
+
+class ParticleForceRegister {
+
+private:
+	std::vector<ForceRecord> register_;
+
+public:
+
+	void add(Particle* p, ParticleForceGenerator* fg);
+	void remove();
+	void clear();
+	void updateForces();
+
+	ParticleForceRegister();
+	~ParticleForceRegister();
+};
+
+#endif
