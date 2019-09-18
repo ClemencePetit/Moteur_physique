717bfc6fbcd78c7f707652a0a0278bc92684c837
Ensuque <khamaz.ee@gmail.com>
2019-09-18 14:43:48 -0400

    GravityGeneratorForce

---
+++ Moteur_physique/GravityForceGenerator.cpp
@@ -0,0 +1,14 @@
+
+#include "GravityForceGenerator.h"
+
+
+void GravityForceGenerator::updateForce(Particle* p, float t) {
+
+	if (p->getMassInv < 0) {
+
+		p->addForce(
+			gravity_ * (1 / p->getMassInv())
+		);
+
+	}
+}
--- Moteur_physique/ParticleForceGenerator.h
+++ Moteur_physique/ParticleForceGenerator.h
@@ -1,8 +1,13 @@
-#pragma once
+#ifndef PARTICLE_FORCE_GENERATOR
+#define PARTICLE_FORCE_GENERATOR
+
 #include "Particle.h"
 
 //Interface des générateurs de force pour les particules
 class ParticleForceGenerator {
 
-	virtual void updateForce(Particle* particle, float t) = 0;
+public:
+	virtual void updateForce(Particle* p, float t) = 0;
 };
+
+#endif
