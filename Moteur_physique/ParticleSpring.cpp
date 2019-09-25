c2760630f9b18c152b23273d2142b0e71c97de99
Ensuque <khamaz.ee@gmail.com>
2019-09-25 14:29:06 -0400

    simple Spring force added

---
+++ Moteur_physique/ParticleSpring.cpp
@@ -0,0 +1,11 @@
+#include "ParticleSpring.h"
+
+
+void ParticleSpring::updateForce(Particle* p, float t) {
+
+	Vector3D d = *p->getPos() - *other_->getPos();
+
+	p->addForce(
+		d.normalized()  * -k_ * (d.norm() - l0_));
+
+}
