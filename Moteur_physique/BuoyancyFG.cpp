702d38ee2d1d5544dda2c83fd237d7dacf967c28
Martin <martin.chapuis@live.fr>
2019-09-25 15:31:04 -0400

    Modif buoyancy

--- Moteur_physique/BuoyancyFG.cpp
+++ Moteur_physique/BuoyancyFG.cpp
@@ -12,15 +12,12 @@ BuoyancyFG::~BuoyancyFG()
 void BuoyancyFG::updateForce(Particle* p, float t) {
 	if (p->getPos()->z > (s_ + zWater_))
 	{
-		cout << "out" << endl;
 		return;
 	}
 	else if (p->getPos()->z <= (zWater_ - s_)) {
-		cout << "in" << endl;
 		p->addForce(Vector3D(0, 0, v_ * density_));
 	}
 	else{
-		cout << "middle" << endl;
 		float propWater = (p->getPos()->z - s_ - zWater_) / (2 * s_);
 		p->addForce(Vector3D(0, 0, v_ * density_*propWater));
 	}
