09bd3ffee16d4892954d942f2b62e260a7b6b54f
Ensuque <khamaz.ee@gmail.com>
2019-10-08 12:53:02 -0400

    other forumla coll in comments

--- Moteur_physique/ParticleContact.cpp
+++ Moteur_physique/ParticleContact.cpp
@@ -13,33 +13,59 @@ ParticleContact::~ParticleContact()
 
 void ParticleContact::resolve(float t) {
 	resolveInterpenetration();
-	resolveVelocity(t);
+	resolveVelocity();
 }
 
 float ParticleContact::calculVs() {
+
+	Vector3D closingVelocityA = particles_[0]->getVit();
 	//Cas d'un sol
 	if (particles_[1] == NULL) {
-		return (particles_[0]->getVit().dotProd(n_));
+
+		return closingVelocityA.dotProd(n_);
+		
+		/*
+		Vector3D closingVelocityA = particles_[0]->getVit();
+		float j = (closingVelocityA).dotProd(n_) * restit_;
+		return j; */
 	}
 	//Cas de 2 particules
 	else {
-		return ((particles_[0]->getVit() - particles_[1]->getVit()).dotProd(n_));
+
+		Vector3D closingVelocityB = particles_[1]->getVit();
+		return (closingVelocityA - closingVelocityB).dotProd(n_);
+
+		
+		/*
+		float factor = (-1 + restit_) / (particles_[0]->getMassInv() + particles_[1]->getMassInv());
+		float j = (closingVelocityA - closingVelocityB).dotProd(n_) * factor; 
+		return j; */
 	}
 }
 
-void ParticleContact::resolveVelocity(float t) {
+void ParticleContact::resolveVelocity() {
 
 	if (vs_ <= 0) return; //ils ne se rapprochent pas et non donc pas besoin d'etre �loign�s
 
-	float vTemp = restit_ * vs_;
+	float vsP = -restit_ * vs_;
+
 	//Cas d'un sol
 	if (particles_[1] == NULL) {
-		particles_[0]->setVit(particles_[0]->getVit() + n_ * -vTemp * t);
+
+		particles_[0]->setVit(particles_[0]->getVit() + n_ * vsP);
+
+		//particles_[0]->setVit(particles_[0]->getVit() + n_* vs_ * particles_[0]->getMassInv());
 	}
 	//Cas de 2 particules
 	else {
-		particles_[0]->setVit(particles_[0]->getVit() + n_ * -vTemp * t);
-		particles_[1]->setVit(particles_[1]->getVit() + n_ * -vTemp * t);
+
+
+		particles_[0]->setVit(particles_[0]->getVit() + n_ * vsP * particles_[0]->getMassInv());
+		particles_[1]->setVit(particles_[1]->getVit() - n_ * vsP * particles_[1]->getMassInv());
+
+		//particles_[0]->setVit(particles_[0]->getVit() + n_ * vs_ * particles_[0]->getMassInv());
+		//particles_[1]->setVit(particles_[1]->getVit() - n_ * vs_ * particles_[1]->getMassInv());
+
 	}
 }
 
@@ -54,7 +80,7 @@ void ParticleContact::resolveInterpenetration() {
 		float mb = 1 / particles_[1]->getMassInv();
 
 		Vector3D deltaPA = n_ * (mb / (ma + mb)) * dPene_;
-		Vector3D deltaPB = n_ * -1 * (ma / (ma + mb)) * dPene_;
+		Vector3D deltaPB = n_ * (ma / (ma + mb)) * dPene_ * - 1;
 
 		particles_[0]->setPos(*particles_[0]->getPos() + deltaPA);
 		particles_[1]->setPos(*particles_[1]->getPos() + deltaPB);
--- Moteur_physique/ParticleContact.h
+++ Moteur_physique/ParticleContact.h
@@ -24,7 +24,7 @@ public:
 
 	void resolve(float t);
 	float calculVs();
-	void resolveVelocity(float t);
+	void resolveVelocity();
 	void resolveInterpenetration();
 
 
